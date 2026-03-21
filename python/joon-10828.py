2d7dc046c2176436c7f3f5a321dfe4175f873689
bellround <rainarmy11@gmail.com>
2026-03-21 23:06:00 +0900

    백준 10828번 문제 풀이

--- python/joon-10828.py
+++ python/joon-10828.py
@@ -1,3 +1,22 @@
 a = []
 for i in range(int(input())):
-    b = list(map(int, input().split()))
+    b = list(map(str, input().split()))
+    if b[0] == 'push':
+        a.append(b[1])
+    elif b[0] == 'pop':
+        if len(a) == 0:
+            print(-1)
+        else:
+            print(a.pop())
+    elif b[0] == 'size':
+        print(len(a))
+    elif b[0] == 'empty':
+        if len(a) == 0:
+            print(1)
+        else:
+            print(0)
+    else:
+        if len(a) == 0:
+            print(-1)
+        else:
+            print(a[len(a)-1])
