40782b3f82ce9619b531267c7b59b192f9fa1fa1
bellround <rainarmy11@gmail.com>
2026-03-23 19:16:31 +0900

    백준 4153번 문제 풀이

---
+++ python/joon-4153.py
@@ -0,0 +1,10 @@
+# 직각삼각형
+while 1:
+    a = list(map(int, input().split()))
+    a.sort()
+    if a[0] == 0 and a[1] == 0 and a[2] == 0:
+        break
+    elif a[0] ** 2 + a[1] ** 2 == a[2] ** 2:
+        print('right')
+    else:
+        print('wrong')
