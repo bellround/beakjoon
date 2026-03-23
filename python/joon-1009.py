ae4864d13f2bcf41bdd1211c7de32cd428e54645
bellround <rainarmy11@gmail.com>
2026-03-23 16:49:52 +0900

    백준 1009번 문제 풀이

--- python/joon-1009.py
+++ python/joon-1009.py
@@ -3,4 +3,5 @@ input = sys.stdin.readline
 n = int(input())
 for i in range(n):
     a, b = map(int, input().split())
-    print((a ** b)%10)
+    m = (a ** ( (b - 1) % 4 + 1 ))%10
+    print(10 if m == 0 else m)
