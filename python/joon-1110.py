6b2e69b1f8756ba7170bf0d47827ec865d4988eb
bellround <rainarmy11@gmail.com>
2026-03-29 22:43:02 +0900

    백준 1110번 문제풀이

--- python/joon-1110.py
+++ python/joon-1110.py
@@ -3,7 +3,7 @@ a = b # 복사
 i = 0  # 몇번 반복했는지
 while 1:
     i += 1
-    a = str((int(a)%10 * 10))[0] + str(int(a[0])+int(a[1]))[-1] # 계산식
-    if a == b: # 같으면 나가기
+    a = str((int(a)%10 * 10))[0] + str(int(a[-1])+int(a[-2]))[-1] # 계산식
+    if int(a) == int(b): # 같으면 나가기
         break
 print(i)
