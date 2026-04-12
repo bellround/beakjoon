6216dbca4c69f5985697515057fded1e9e16ff48
bellround <rainarmy11@gmail.com>
2026-04-12 22:16:43 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,12 @@
+group = ['','D0','D+','C0','C+','B0','B+','A0','A+']
+n = 0
+m = 0
+for i in range(20):
+    a, b, c = map(str, input().split())
+    if c in group:
+        m += float(b)
+        n += float(b) * ((group.index(c)+1) / 2)
+    else:
+        if c=='F':
+            m += float(b)
+print(round(n/m,6))
